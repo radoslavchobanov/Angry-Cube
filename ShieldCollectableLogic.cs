79341468d9f4d329daa0024cf52c802f10979e37
Radoslav Chobanov <qguarat@gmail.com>
2017-03-24 19:58:30 +0200

    Add files via upload

---
+++ ShieldCollectableLogic.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldCollectableLogic : MonoBehaviour {
+
+	public GameObject shieldGO;
+	public AudioClip shieldSound;
+
+	void Start()
+	{
+		Destroy (this.gameObject, 6f);
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.gameObject.tag == "Player")
+		{
+			AudioSource.PlayClipAtPoint(shieldSound, new Vector3(0,0,0));
+			Instantiate(shieldGO);
+			Destroy(this.gameObject);
+		}
+	}
+}
