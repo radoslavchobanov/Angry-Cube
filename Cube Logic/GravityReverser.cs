617a3a17bf555e8d4f0586f1e2f679c46150883a
Radoslav Chobanov <qguarat@gmail.com>
2017-03-24 20:20:33 +0200

    Add files via upload

---
+++ Cube Logic/GravityReverser.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityReverser : MonoBehaviour 
+{
+	public static void GravityReverse()
+	{
+		Physics2D.gravity = new Vector2 (0, -10);
+	}
+
+	public static void NormalGravity()
+	{
+		Physics2D.gravity = new Vector2 (0, 10);
+	}
+}
