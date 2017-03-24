261ac8558184eb123ad889ff64e734ddf8cf0ef7
Radoslav Chobanov <qguarat@gmail.com>
2017-03-24 20:21:37 +0200

    Add files via upload

---
+++ Flying Object Logic/ShieldLogic.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldLogic : MonoBehaviour 
+{
+
+	void Start()
+	{
+		this.gameObject.transform.SetParent (GameObject.FindGameObjectWithTag("Player").transform);
+		this.gameObject.transform.localPosition = Vector3.zero;
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.gameObject.name == "Rock" || col.gameObject.name == "Rock(Clone)")
+		{
+			Destroy(col.gameObject);
+			Destroy(this.gameObject);
+		}
+	}
+}
